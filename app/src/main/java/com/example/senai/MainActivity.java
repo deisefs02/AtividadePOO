c85acf6aee54836fddfdeae5aeb7d206041c693f
DEISE <deisefs02@gmail.com>
2021-05-09 09:57:53 -0300

    APP_POO

---
+++ app/src/main/java/com/example/senai/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.senai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickBtnMostrar(View v){
+        EditText editText = findViewById(R.id.et_texto);
+        EditText editText2 = findViewById(R.id.et_texto2);
+
+        String Texto1 = editText.getText().toString();
+        String Texto2 = editText2.getText().toString();
+        String completo = Texto1 + " " + Texto2;
+
+        Toast.makeText(MainActivity.this, completo, Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickBtnLimpar(View v){
+        EditText editText = findViewById(R.id.et_texto);
+        EditText editText2 = findViewById(R.id.et_texto2);
+
+        editText.getText().clear();
+        editText2.getText().clear();
+    }
+    }
